326a4ad08a7f12368440ab3440efa9177a4fc496
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-10-01 23:30:23 -0300

    skeleton page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
 import { useState } from 'react'
-import './App.css'
+import './index.css'
 
-function App() {
+const App: React.FC = () => {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1 className='text-2xl text-white font-bold'>WELCOME</h1>
+    <div className='bg-white'>
+      <header>
+        <div>
+          <h1 className='text-2xl font-bold'>WELCOME</h1>
+
+        </div>
+
+      </header>
+      
+      <main>
+          <div>
+            <p className='text-black'>Count teste: {count}</p>
+            <button 
+              className='bg-blue-500 text-white p-2 rounded'
+              onClick={() => setCount(count + 1)}
+            >Increment
+            </button>
+          </div>
+      </main>
+
+      <footer>
+        <div>
+          <p>Links vão ser criador aqui</p>
+        </div>
+      </footer>
+    </div>
     </>
 
   )
