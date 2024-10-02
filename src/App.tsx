f4c1c438f0d54f7da16b93499c55632cf0924a7f
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-10-01 22:20:14 -0300

    typescript install and init

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,9 +7,6 @@ function App() {
   return (
     <>
      <h1 className='text-2xl text-white font-bold'>WELCOME</h1>
-
-     
-
     </>
 
   )
