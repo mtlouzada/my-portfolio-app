248faeaea8d3d8b732e87f3a40842f1ee6f72946
mt.louzada <matheuslouzadaa@gmail.com>
2024-10-19 22:54:10 -0300

    new: header skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,38 @@ function App() {
     <>
       <div>
 
-        <div className='menu-bar'>
-          <div>
-            <a href="">matheuslouzadaa@gmail.com</a>
-              <a href="">Copy Email</a>
+
+        <header>
+          <div className='menu-bar'>
+            <div>
+              <a href="">matheuslouzadaa@gmail.com</a>
+                <a href="">CV</a>
+            </div>
+            <div>
+              <a href="https://www.linkedin.com/in/matheus-louzadaa/" target='black'>LinkedIn</a>
+              <a href="https://github.com/mtlouzada" target='black'>GitHub</a>
+            </div>
           </div>
+
           <div>
-            <a href="https://www.linkedin.com/in/matheus-louzadaa/" target='black'>LinkedIn</a>
-            <a href="https://github.com/mtlouzada" target='black'>GitHub</a>
+            <img src="./perfil-project.jpeg" alt="Foto Perfil"/>
+            <h1>Matheus Louzada</h1>
+            <h3>Desenvolvedor web</h3>
+
+            <p>
+            Me formei em ADS no fim de 2023, desde então estou fazendo cursos profissionalizantes focado no desenvolvimento Web2. Também participei de um BootCamp seguido de um Hackathon na ICP HUB Brasil, onde tive a experiência de desenvolver um projeto em equipe e apresentá-lo.
+
+Estudo sobre programação diariamente, me desafiando a criar projetos completos, utilizando bibliotecas, frameworks e banco de dados para isso. Desenvolvendo autonomia na criação de código e aprimorando o meu raciocínio lógico. Visando ser capaz de entregar soluções tecnológicas de forma rápida e eficaz.
+            </p>
+
+            <a href="">Ver Currículo</a>
+            <a href="">Enviar Email</a>
+
           </div>
-        </div>
+
+        </header>
+
+
 
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
